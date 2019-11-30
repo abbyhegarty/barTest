80c0d30cd7e1157c1586a8f264ac17bf4a8356ee
abbyhegarty <54848927+abbyhegarty@users.noreply.github.com>
2019-11-30 16:08:48 -0500

    Create barGraph.js

---
+++ barGraph.js
@@ -0,0 +1,10 @@
+var sampleData= [{name: "fred", rise: 12, run: 5, width: 6},
+                {name: "sam", rise: 15, run: 7, width: 7},
+                {name: "pebbles", rise: 18, run: 11, width: 9},
+                {name: "barney", rise: 8, run: 15, width: 6},
+                {name: "george", rise: 9, run: 17, width: 10},
+                {name: "green", rise: 13, run: 19, width: 3},
+                {name: "orange", rise: 5, run: 15, width: 2},
+                {name: "red", rise: 3, run: 12, width: 1},
+                {name: "yellow", rise: 1, run: 3, width: 5},
+                {name: "purple", rise: 12, run: 7, width: 6},]
