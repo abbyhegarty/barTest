6c1bc40d219a32edb096f9b1274e71f017059a16
abbyhegarty <54848927+abbyhegarty@users.noreply.github.com>
2019-12-02 16:51:29 -0500

    Update barGraph.js

--- barGraph.js
+++ barGraph.js
@@ -8,3 +8,49 @@ var sampleData= [{name: "fred", rise: 12, run: 5, width: 6},
                 {name: "red", rise: 3, run: 12, width: 1},
                 {name: "yellow", rise: 1, run: 3, width: 5},
                 {name: "purple", rise: 12, run: 7, width: 6},]
+
+
+
+var setup = function(sampleData)
+{
+    var screen = {width: 800, height: 500}
+    
+    var margins = {top: 25, bottom: 35, left: 50, right: 25}
+    
+    var width = screen.width - margins.left - margins.right
+    var height = screen.height - margins.top - margins.bottom
+    
+    var xScale = d3.scaleLinear()
+                    .domain([0,20])
+                    .range([0, width])
+    
+    var yScale = d3.scaleLinear()
+                    .domain([0,20])
+                    .range([height-5,0])
+    
+    var xAxis = d3.axisBottom(xScale)
+    var yAxis = d3.axisLeft(yScale)
+    
+     svg.append("g")
+    .attr("id","xAxis")
+    .attr("transform","translate("+margins.left+","+(margins.top+height)+")")
+    .call(xAxis)
+    
+    svg.append("text")
+    .attr("transform", "translate("+ (width/2)+","+(height+margins.top+30)+")")
+    .style("text-anchor", "middle")
+    .text("Horizontal Axis")
+    
+    svg.append("g")
+    .attr("id","yAxis")
+    .attr("transform","translate(35,"+margins.top+")")
+    .call(yAxis);
+    
+    svg.append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", (margins.left-55))
+      .attr("x", 0-(height / 2))
+      .attr("dy", "1em")
+      .style("text-anchor", "middle")
+      .text("Vertical Axis");        
+    }
